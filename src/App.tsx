8b94e324bc442f97deee103e1891daeb8d3b633c
ThomasMerritt <84432380+ThomasMerritt@users.noreply.github.com>
2025-06-08 20:48:40 -0700

    get on

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import dance from '../src/assets/dance.gif';
+import music from '../src/assets/motivationalMusic.mp3';
 
 function App() {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    setFadeIn(true); 
+
+    const audio = new Audio(music);
+    audio.loop = true;
+
+    const playAudio = async () => {
+          try {
+              await audio.play();
+          } catch (error) {
+              console.warn("Audio autoplay blocked:", error);
+          }
+      };
+
+    playAudio();
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`main-content ${fadeIn ? 'fade-in' : ''}`}>
+      <img src={dance} alt="Dancing gif" />
+      <h1 className='title'>Get on Nightreign</h1>
     </div>
   );
 }
